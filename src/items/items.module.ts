70b8faa4b3909e9afd09fb0dcdc4eee26f3fbee0
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-21 09:59:57 -0600

    feat: adding items module

---
+++ src/items/items.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { ItemsService } from './items.service';
+import { ItemsResolver } from './items.resolver';
+
+@Module({
+  providers: [ItemsResolver, ItemsService],
+  imports: [TypeOrmModule.forFeature([Item])],
+})
+export class ItemsModule {}
